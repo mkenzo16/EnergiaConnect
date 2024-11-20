dad134c8d213df9befdd20e273f940c22cb67c0b
mkenzo16 <kenzotakayama1605@gmail.com>
2024-11-20 04:32:28 -0300

    alteração no cabecalho

--- energia-connect/src/app/components/Cabecalho/Header.tsx
+++ energia-connect/src/app/components/Cabecalho/Header.tsx
@@ -1,46 +1,46 @@
+"use client";
+
 import React from 'react';
-import Link from 'next/link'; 
+import Link from 'next/link';
 import styles from './Header.module.css';
 
 const Header = () => {
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
-        <img src="/logo.png" alt="Logo" className={styles.logoImage} />
+        <img src="/logo.png" alt="Logo" />
       </div>
       <nav>
         <ul className={styles.navLinks}>
-          <li>
-            <Link href="/" className={styles.navLink}>
-              Início
-            </Link>
-          </li>
-          <li>
-            <Link href="#about" className={styles.navLink}>
-              Serviços
-            </Link>
-          </li>
-          <li>
-            <Link href="#partners" className={styles.navLink}>
-              Preços
-            </Link>
-          </li>
-          <li>
-            <Link href="#footer" className={styles.navLink}>
-              Sobre nós
-            </Link>
-          </li>
-          <li>
-            <Link href="/contato" className={styles.contactButton}>
-              Entre em contato
-            </Link>
-          </li>
+          <li><Link href="/">Início</Link></li>
+          <li><Link href="/#services">Serviços</Link></li>
+          <li><Link href="/#pricing">Preços</Link></li>
+          <li><Link href="/#about">Sobre nós</Link></li>
+          <li><Link href="/contato" className={styles.contactButton}>Entre em contato</Link></li>
         </ul>
       </nav>
+      <div className={styles.actions}>
+        <div className={styles.searchContainer}>
+          <input
+            type="text"
+            placeholder="Pesquisar..."
+            className={styles.searchInput}
+          />
+        </div>
+        <div className={styles.userIconContainer}>
+          <Link href="/cadastro">
+            <button className={styles.userIcon}>
+              <img src="/user.png" alt="User" />
+            </button>
+          </Link>
+        </div>
+      </div>
     </header>
   );
 };
 
 export default Header;
+
+
 
 
